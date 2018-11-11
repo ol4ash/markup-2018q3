dbc819143c145301f0a63801a77a609b3468cf08
ol4ash <shaukavets@gmail.com>
2018-11-11 02:22:55 +0200

    feat: add notification window; add css for it; add close button;

--- notifications/notify.js
+++ notifications/notify.js
@@ -1,3 +1,21 @@
 
-alert (" hi");
 
+setTimeout(function(){show();}, 2000);
+
+var show = function() {
+    return document.getElementById("mydiv").className = "notificationVisible"
+    }
+var hide = function() {
+    return document.getElementById("mydiv").className = "notification"
+    }
+
+
+
+// let arr = [
+// "1",
+// "2",
+// "3",
+// "4",
+// "5",
+// "6"
+// ];
